e92b69b42a3659dda6c7916913f22614365989f8
Amjeed <ayvmax@gmail.com>
2024-09-07 17:10:31 +0100

    fix sw a

--- assets/app.js
+++ assets/app.js
@@ -1,20 +1,3 @@
-// Register Service Worker
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function () {
-    navigator.serviceWorker.register("../service-worker.js").then(
-      function (registration) {
-        console.log(
-          "Service Worker registered with scope:",
-          registration.scope
-        );
-      },
-      function (err) {
-        console.log("Service Worker registration failed:", err);
-      }
-    );
-  });
-}
-
 function playMusic(song, songId) {
   const audioPlayer = document.getElementById("audio-player");
   const audioSource = document.getElementById("audio-source");
